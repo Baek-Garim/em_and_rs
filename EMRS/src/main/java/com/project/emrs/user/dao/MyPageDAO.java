1ca2e67170845f36f7e3a4841fee528fa5c6b20d
rkfla <rkfla@BOOK-1FN470O3GE>
2024-07-18 20:23:01 +0900

    헤더 마이페이지 연결, 마이페이지 미완성

--- EMRS/src/main/java/com/project/emrs/user/dao/MyPageDAO.java
+++ EMRS/src/main/java/com/project/emrs/user/dao/MyPageDAO.java
@@ -2,10 +2,13 @@ package com.project.emrs.user.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.project.emrs.user.dto.UserDTO;
+
 @Mapper
 public interface MyPageDAO {
-	
-	
+
+	UserDTO selectUser(Integer user_id);
+
 	
 	
 } // DAO
--- EMRS/src/main/java/com/project/emrs/user/service/MyPageService.java
+++ EMRS/src/main/java/com/project/emrs/user/service/MyPageService.java
@@ -1,7 +1,9 @@
 package com.project.emrs.user.service;
 
+import com.project.emrs.user.dto.UserDTO;
+
 public interface MyPageService {
-	
-	
-	
+
+	UserDTO selectUser(Integer user_id);
+
 } // Service
