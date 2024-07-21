cd37fd62a63d552c4aa99d7931a8437b5d8435f5
rkfla <rkfla@BOOK-1FN470O3GE>
2024-07-21 22:22:36 +0900

    RENTAL 날짜처리 HTML로 변경

--- EMRS/src/main/java/com/project/emrs/user/controller/MyPageController.java
+++ EMRS/src/main/java/com/project/emrs/user/controller/MyPageController.java
@@ -81,17 +81,6 @@ public class MyPageController {
 		}
 		
 		ArrayList<RentalDTO> rentalList = rentalService.rentalList((Integer)session.getAttribute("user_id"));
-
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년MM월dd일");
-		for(RentalDTO i: rentalList) {
-			i.setRental_date_String(i.getRental_date().format(formatter));
-			i.setExpected_return_date_String(i.getExpected_return_date().format(formatter));
-			if (i.getRental_date() == null) {
-	            i.setRental_date_String(" ");
-	        } else {
-	        	i.setReturn_date_String(i.getReturn_date().format(formatter));
-	        }
-        }
 		
 		// 카테고리		
 		ArrayList<ToolCategoryDTO> categoryList = toolService.getAllCategory();
